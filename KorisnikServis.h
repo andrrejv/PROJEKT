7ada51403d2f743c3c09758294093b4326b5d3ed
andrrejv <andrrejv.4@gmail.com>
2025-06-06 08:42:20 +0200

    Update KorisnikServis.h

--- KorisnikServis.h
+++ KorisnikServis.h
@@ -9,6 +9,22 @@ void ispisiSveKorisnike(void);
 Korisnik* dohvatiSveKorisnike(int* brojKorisnika);
 void obrisiKorisnika(Korisnik korisnik);
 
+inline char* dohvatiString(char* ispisnaVrijednost) {
+    char buffer[1024];
+    printf("%s", ispisnaVrijednost);
+    fgets(buffer, sizeof(buffer), stdin);
+
+    buffer[strcspn(buffer, "\n")] = 0;
+
+    char* ime = malloc(strlen(buffer) + 1);
+    if (!ime) {
+        perror("Greška pri alokaciji memorije");
+        exit(1);
+    }
+    strcpy(ime, buffer);
+    return ime;
+}
+
 extern int brojKorisnika;
 char* upisiIme(void);
 char* upisiAdresu(void);
