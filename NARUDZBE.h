46c77f685b38c3d6aa212688f12723e5742cfd17
andrrejv <andrrejv.4@gmail.com>
2025-06-16 22:17:42 +0200

    Update NARUDZBE.h

--- NARUDZBE.h
+++ NARUDZBE.h
@@ -1,13 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #ifndef NARDUZBE_H
 #define NARDUZBE_H
 
 #include "SLOZENI_TIPOVI.h"
 
 void dodajNarudzbu(void); 
-void ispisiSveNarudzbe(void);
+int ispisiSveNarudzbe(bool saIzborom);
 Narudzba* dohatiSveNarudzbe(int* brojNarudzbi); 
-void obrisiNarudzbu(Narudzba narduzba); 
-
+void obrisiNarudzbu(void); 
 MotoOprema odaberiOpremu(MotoOprema* sveOpreme, int brojOpreme);
 int odaberiKorisnika(Korisnik* sviKorisnici, int brojKorisnika);
 #endif
